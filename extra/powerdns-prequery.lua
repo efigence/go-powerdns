70454c1cc6a70274a320cdd4926b7fe019a703b2
Mariusz Gronczewski <xani666@gmail.com>
2015-12-21 14:48:02 +0100

    add batch load

--- extra/powerdns-prequery.lua
+++ extra/powerdns-prequery.lua
@@ -11,28 +11,29 @@ function prequery ( dnspacket )
    pdnslog ("q: ".. qname.." "..qtype)
    if lastRedirUpdate + 60 < os.time() then
       result, statuscode, content = http.request("http://127.0.0.1:63636/redir/list")
-      if pcall(getIpList(content)) then
+      if pcall(function() getIpList(content) end) then
          pdnslog ("updating IP list")
+         lastRedirUpdate = os.time()
       else
          pdnslog ("failed to update IP list")
       end
    end
 
-    if qtype == pdns.A and remote_ip=="192.168.241.5"
-    then
-        pdnslog ("calling dnspacket:setRcode")
---      dnspacket:setRcode(pdns.NXDOMAIN)
-        pdnslog ("called dnspacket:setRcode")
-        pdnslog ("adding records")
-        ret = {}
-        ret[1] = {qname=qname, qtype=qtype, content="1.2.3.4", place=2, ttl=10}
-        ret[2] = {qname=qname, qtype=pdns.TXT, content=os.date("Retrieved at %Y-%m-%d %H:%M:%S"), ttl=10}
-        dnspacket:addRecords(ret)
-        pdnslog ("returning true")
-        return true
-    end
-    pdnslog ("returning false")
-    return false
+   if qtype == pdns.A and redirList[remote_ip]
+   then
+      pdnslog ("calling dnspacket:setRcode")
+      --      dnspacket:setRcode(pdns.NXDOMAIN)
+      pdnslog ("called dnspacket:setRcode")
+      pdnslog ("adding records")
+      ret = {}
+      ret[1] = {qname=qname, qtype=qtype, content=redirList[remote_ip], place=2, ttl=10}
+      ret[2] = {qname=qname, qtype=pdns.TXT, content=os.date("Retrieved at %Y-%m-%d %H:%M:%S"), ttl=10}
+      dnspacket:addRecords(ret)
+      pdnslog ("returning true")
+      return true
+   end
+   pdnslog ("returning false")
+   return false
 end
 
 
--- webapi/redir.go
+++ webapi/redir.go
@@ -3,6 +3,8 @@ package webapi
 import (
 	"net/http"
 	"github.com/zenazn/goji/web"
+	"io/ioutil"
+	"encoding/json"
 )
 
 var	respOk = map[string]string {
@@ -24,7 +26,15 @@ func (w *WebApp) AddRedir(c web.C, wr http.ResponseWriter, r *http.Request) {
 
 
 }
-func (w *WebApp) BatchAddRedir(c web.C, wr http.ResponseWriter, r *http.Request) {}
+func (w *WebApp) BatchAddRedir(c web.C, wr http.ResponseWriter, r *http.Request) {
+	raw,err := ioutil.ReadAll(r.Body)
+	if (err != nil) {return}
+	ipList := make(map[string]string)
+	err = json.Unmarshal(raw, &ipList)
+	if (err != nil) {return}
+	err = w.dnsBackend.redirBackend.SetRedirIp(ipList)
+	w.render.JSON(wr, http.StatusOK, respOk)
+}
 func (w *WebApp) DeleteRedir(c web.C, wr http.ResponseWriter, r *http.Request) {
 	from := c.URLParams["from"]
 	err := w.dnsBackend.redirBackend.DeleteRedirIp(from)
